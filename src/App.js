0484e5d7278546ef2fc2153dd8bb121369643339
dhshin <dhshin.main@gmail.com>
2024-02-19 11:53:01 +0900

    upload react codes

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import RequestChart from './RequestChart';
+import NodeChart from './NodeChart';
+import './App.css';
+import jsonData from './record_list.json';
+import nodeData from './SiouxFalls_node.csv';
+
+function App() {
+    const [sortOption, setSortOption] = useState('none');
+
+    const handleSortChange = (event) => {
+        setSortOption(event.target.value);
+    };
+
+    return (
+        <div>
+            <nav style={{ height: '50px', background: 'white', margin: '4px' }}>
+                <span style={{ marginLeft: '10px', fontSize: '1.5rem' }}>DRT-Explorer</span>
+            </nav>
+
+            <div>
+                <label htmlFor="sort">Sort : </label>
+                <select id="sort" onChange={handleSortChange} value={sortOption}>
+                    <option value="none">none</option>
+                    <option value="ascending">ascending</option>
+                    <option value="descending">descending</option>
+                </select>
+            </div>
+
+            <div
+                className="App"
+                style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100%' }}
+            >
+                <RequestChart recorddata={jsonData} sortOption={sortOption} />
+                <NodeChart recorddata={jsonData} nodeData={nodeData} />
+            </div>
+        </div>
+    );
+}
+
+export default App;
