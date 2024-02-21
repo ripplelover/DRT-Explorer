8bf05a6b4dbb31b077480b364fb6b55567a9657a
ripplelover <devin0626@naver.com>
2024-02-21 11:23:23 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,36 +6,36 @@ import jsonData from './record_list.json';
 import nodeData from './SiouxFalls_node.csv';
 
 function App() {
-    const [sortOption, setSortOption] = useState('none');
+  const [sortOption, setSortOption] = useState('none');
 
-    const handleSortChange = (event) => {
-        setSortOption(event.target.value);
-    };
+  const handleSortChange = (event) => {
+    setSortOption(event.target.value);
+  };
 
-    return (
-        <div>
-            <nav style={{ height: '50px', background: 'white', margin: '4px' }}>
-                <span style={{ marginLeft: '10px', fontSize: '1.5rem' }}>DRT-Explorer</span>
-            </nav>
+  return (
+    <div>
+      <nav style={{ height: '50px', background: 'white', margin: '4px' }}>
+        <span style={{ marginLeft: '10px', fontSize: '1.5rem' }}>DRT-Explorer</span>
+      </nav>
+      <div>
+        <label htmlFor="sort">Sort : </label>
+        <select id="sort" onChange={handleSortChange} value={sortOption}>
+          <option value="none">none</option>
+          <option value="ascending">ascending</option>
+          <option value="descending">descending</option>
+        </select>
+      </div>
+      <div
+        className="App"
+        style={{ display: 'flex', justifyContent: 'center', width: '100%' }}
+      >
+        <RequestChart recorddata={jsonData} sortOption={sortOption} />
+        <NodeChart recorddata={jsonData} nodeData={nodeData} />
+      </div>
+      <div style={{ display: 'flex' , height: '100px'}}></div>
 
-            <div>
-                <label htmlFor="sort">Sort : </label>
-                <select id="sort" onChange={handleSortChange} value={sortOption}>
-                    <option value="none">none</option>
-                    <option value="ascending">ascending</option>
-                    <option value="descending">descending</option>
-                </select>
-            </div>
-
-            <div
-                className="App"
-                style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100%' }}
-            >
-                <RequestChart recorddata={jsonData} sortOption={sortOption} />
-                <NodeChart recorddata={jsonData} nodeData={nodeData} />
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default App;
